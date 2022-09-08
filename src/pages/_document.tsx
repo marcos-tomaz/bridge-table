e89496f159631ec005b2137f1c7bf10e54815d7f
Marcos Tomaz <marcoshmpm@gmail.com>
2022-09-07 21:40:40 -0300

    setup app root provider component

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Domine:wght@400;500&family=Roboto:wght@100;400;500;700&display=swap'
+          rel='stylesheet'
+        ></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
